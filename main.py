1143f8b413c87665bba8bae6ce4530b1dcd1dad7
Pixelgon <pixelgon4@gmail.com>
2022-06-17 12:59:20 +0200

    First 2

--- main.py
+++ main.py
@@ -3,22 +3,31 @@ file = open(name, "r")
 filedic = {}
 ukon = int(input("Zadejte co chcete dělat(1 sifrovat, 2 desifrovat, 3 overit):"))
 
-for x in file:
-    key, value = x.split()
-    filedic[key] = value
 
-def cypher(text, filedic, file):
+def cypher(text, filedic):
     for char, initial in filedic.items():
-        text = text.replace(char.lower(), initial)
-    file.close()
+        text = text.replace(char, initial)
     return text
 
+def overeni_klicu(dic):
+
+
 if ukon == 1:
-    text = str(input("Zadejte text: "))
-    print(cypher(text, filedic, file))
+    for x in file:
+        key, value = x.split()
+        filedic[key] = value
+    file.close()
+    text = str(input("Zadejte text:"))
+    print("Zašifrovaný text je:", cypher(text, filedic))
+    exit()
 if ukon == 2:
+    for x in file:
+        value, key = x.split()
+        filedic[key] = value
+    file.close()
     text = str(input("Zadejte text: "))
-    filedic.reverse()
-    print(cypher(text, filedic, file))
+    print("Dešifrovaný texte je:", cypher(text, filedic))
+    exit()
 if ukon == 3:
+    print(overeni_klicu(filedic))
     exit()
